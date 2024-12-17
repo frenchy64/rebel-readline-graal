9fc3e7ae5cc327201d4a43961b0ce059812e2f9d
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2024-12-17 01:03:51 -0600

    [skip ci]

--- rebel-readline/build.clj
+++ rebel-readline/build.clj
@@ -1,11 +1,13 @@
 (ns build
-  (:require [clojure.tools.build.api :as b]))
+  (:require [clojure.tools.build.api :as b]
+            [deps-deploy.deps-deploy :as dd]))
 
 (def lib 'com.ambrosebs/rebel-readline)
 (def class-dir "target/classes")
 (def version "0.1.5-SNAPSHOT")
 (def basis (delay (b/create-basis {:project "deps.edn"})))
-(def uber-file (format "target/%s-standalone.jar" (name lib)))
+(def uber-file (format "target/%s-%s-standalone.jar" (name lib) version))
+(def jar-file (format "target/%s-%s.jar" (name lib) version))
 (def main-ns 'rebel-readline.main)
 (def src-dirs ["src" "resources"])
 (def java-dirs ["java"])
@@ -17,7 +19,7 @@
   (b/javac (cond-> {:src-dirs java-dirs
                     :class-dir class-dir
                     :basis @basis}
-             (not native) (assoc :javac-opts ["-source" "8" "-target" "8"]))))
+             (not native) (assoc :javac-opts ["--release" "8"]))))
 
 (defn prep [_]
   (compile-java nil))
@@ -38,3 +40,23 @@
              :uber-file uber-file
              :basis basis
              :main main-ns})))
+
+(defn jar [_]
+  (b/write-pom {:class-dir class-dir
+                :lib lib
+                :version version
+                :basis basis
+                :src-dirs ["src"]})
+  (b/copy-dir {:src-dirs ["src" "resources"]
+               :target-dir class-dir})
+  (b/jar {:class-dir class-dir
+          :jar-file jar-file}))
+
+(defn deploy [opts]
+  (jar opts)
+  ((requiring-resolve 'deps-deploy.deps-deploy/deploy)
+    (merge {:installer :remote
+                       :artifact jar-file
+                       :pom-file (b/pom-path {:lib lib :class-dir class-dir})}
+                    opts))
+  opts)
